32ebf3493f80fc102746cd489e938ccdead33cd5
Penélope Carolina <89213449+pnlpcarolina@users.noreply.github.com>
2021-10-15 16:16:42 -0300

    sobre variáveis

---
+++ sobreVariaveis.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+
+# Variáveis é o nome que se refere a um valor dando um comando de atribuição - que pode ser inteiro, string ou float (numero não inteiro)
+variavel1 = "Uma variável pode ter qualquer nome, deve ser escolhido um que faça sentido para quem vai ler o código"
+tipo_de_moradia = "Apartamento"
+numero_andar = 11
+print (variavel1)
+print (tipo_de_moradia, numero_andar)
+
+# Uma variável pode ser alterada 
+variavel1 = "Podemos mudar os valores de referência de uma variável, e o computador deverá imprimir conforme comandamos"
+print(variavel1)
+
+
+# se você altera a variável várias vezes e somente printa ao final, o ultimo valor associado a ela será impresso. 
+tipo_de_moradia = "Casa"
+tipo_de_moradia = "Alojamento"
+tipo_de_moradia= "Hotel"
+
+print(tipo_de_moradia)
