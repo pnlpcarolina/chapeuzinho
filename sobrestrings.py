893719d2cad2cba5ffae326968661a995fb75206
Penélope Carolina <89213449+pnlpcarolina@users.noreply.github.com>
2021-10-15 15:42:46 -0300

    sobre strings

---
+++ sobrestrings.py
@@ -0,0 +1,23 @@
+# Strings é um tipo de dado em sequência de caracteres de letras ou símbolos
+
+# Strings representam valores de caracteres inseridos entre aspas
+print ('\nEsta é uma String')
+print ("Esta também é uma string")
+
+### Aspas triplas podem possuir outras aspas dentro dela
+print ("""\nA Marcela disse: "Ela é bonita!" """)
+print ('''Em uma semana ele já estava revoltado, "Eu não acredito que ela fez isso", ele disse.''')
+
+
+# Valores numérios colocados em strings (entre aspas) também são reconhecidos:
+print ('\n34')
+
+# A operação de adicão (+) em strings possuem a propriedade de CONCATENAR (juntar) as duas strings, como:
+print ('8'+'10') 
+print ('98'+'12')
+# Perceba que os numero não foram somados, mas sim concatenados. 
+
+
+
+
+
